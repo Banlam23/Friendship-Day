d82d63ff6b74a73e238829ff9c8acc73c2297ec5
banlam <banlamjingshai23@gmail.com>
2026-07-21 12:38:45 +0530

    Music

--- src/loading.js
+++ src/loading.js
@@ -4,11 +4,15 @@ export function hideLoading() {
 
     if (!loading) return;
 
+    const home = document.getElementById("home");
     // Wait while the loading animation finishes
     setTimeout(() => {
 
         loading.classList.add("hidden");
 
+        if(home){
+        home.style.display = "flex";
+        }
         // Remove from the document after fade out
         setTimeout(() => {
 
